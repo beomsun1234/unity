6d9476eb91d9938dbecc67d83bafc727996faf7c
beomsun1234 <68090443+beomsun1234@users.noreply.github.com>
2020-10-30 10:57:16 +0900

    Add files via upload

---
+++ Scripts/RandMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandMove : MonoBehaviour
+{
+    Vector3 cube;
+    public float moveSpeed;
+    public float maxValue;
+    public float minValue;
+    public Vector3 default_direction;
+
+    void Start()
+    {
+        cube = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cube.x += moveSpeed * Time.deltaTime;
+        transform.position = cube;
+        if(transform.position.x > maxValue || transform.position.x < minValue)
+        {
+            moveSpeed *= -1;
+        }
+    }
+
+ 
+}
